b477ce3e9164d9948f16a6ff109edf28d450e86d
olloff <olloff@gmail.com>
2015-10-06 23:37:18 +0300

    webskypeenhance.user.js

---
+++ webskypeenhance.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Web Skype Enhance
+// @namespace   webskypeenhance_glhf
+// @include     https://web.skype.com/ru/
+// @version     1
+// @grant       GM_addStyle
+// ==/UserScript==
+
+GM_addStyle(":root .swx .callScreen { display: none; } a:visited { color: #008a99 !important; }");
